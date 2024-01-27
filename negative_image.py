ada02ab90f902aa06470e52a061b764f4d295652
Tanmeet Singh Chhabra <tanmeet.chhabra.0@gmail.com>
2024-01-27 19:14:49 +0530

    Correctly formatted program

--- negative_image.py
+++ negative_image.py
@@ -1,22 +1,24 @@
 from PIL import Image
 
+
 def get_negative_pixel(pixel):
-    negative_pixel = [0,0,0]
+    negative_pixel = [0, 0, 0]
     for i in range(3):
         segment = pixel[i]
-        negative_segment = 255-segment
+        negative_segment = 255 - segment
         negative_pixel[i] = negative_segment
     return tuple(negative_pixel)
 
+
 print("Starting")
 image = Image.open("image.jpg")
-negative_image = Image.new(image.mode,image.size)
-width,height = image.size
+negative_image = Image.new(image.mode, image.size)
+width, height = image.size
 for y in range(height):
     for x in range(width):
-        pixel = image.getpixel((x,y))
+        pixel = image.getpixel((x, y))
         negative_pixel = get_negative_pixel(pixel)
-        negative_image.putpixel((x,y),negative_pixel)
+        negative_image.putpixel((x, y), negative_pixel)
 negative_image.save("converted.png")
 negative_image.close()
 image.close()
